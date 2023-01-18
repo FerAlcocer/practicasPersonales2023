272c1f3a1b14de52969c84e2b264ab5b4f72cfd3
Fernando <fernandoalcocer812@hotmail.com>
2023-01-18 18:56:33 -0300

    Inicio y configuraciones generales

---
+++ ejerciciosHooks/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./estilos/css/reset.css";
+import "./estilos/css/pruebas.css";
+import { Contador } from "./Contador";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Contador.jsx />
+  </React.StrictMode>
+);
